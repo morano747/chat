9bf0542f1a47ad2348c62d555cfd7e0b9e0003d5
Dima <morano747@gmail.com>
2019-06-15 18:04:55 +0300

    First Commit

---
+++ server.py
@@ -0,0 +1,105 @@
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+
+
+class Client(Protocol):
+    ip: str = None
+    login: str = None
+    factory: 'Chat'
+    names = []
+
+    def __init__(self, factory):
+        """
+        Инициализация фабрики клиента
+        :param factory:
+        """
+        self.factory = factory
+
+    def connectionMade(self):
+        """
+        Обработчик подключения нового клиента
+        """
+        self.ip = self.transport.getHost().host
+        self.factory.clients.append(self)
+
+        print(f"Client connected: {self.ip}")
+
+        self.transport.write("Welcome to the chat v0.1\n".encode())
+
+    def dataReceived(self, data: bytes):
+        """
+        Обработчик нового сообщения от клиента
+        :param data:
+        """
+        message = data.decode().replace('\n', '')
+
+        if self.login is not None:
+            server_message = f"{self.login}: {message}"
+            self.factory.notify_all_users(server_message)
+            print(self.names)
+
+        else:
+            if message.startswith("login:"):
+                self.login = message.replace("login:", "")
+
+                if self.login in self.names:
+                    print(self.names)  # показывает список
+                    self.transport.write("But this login is buzy already. Please reconnect to server with correct login :(\n".encode())
+                    self.factory.clients.remove(self)
+
+
+                notification = f"New user connected: {self.login}"
+                self.names.append(self.login)  # добавляет в список пользователей
+                self.factory.notify_all_users(notification)
+                print(notification)
+            else:
+                print("Error: Invalid client login")
+
+    def connectionLost(self, reason=None):
+        """
+        Обработчик отключения клиента
+        :param reason:
+        """
+        self.factory.clients.remove(self)
+        print(f"Client disconnected: {self.ip}")
+
+
+
+class Chat(Factory):
+    clients: list
+
+    def __init__(self):
+        """
+        Инициализация сервера
+        """
+        self.clients = []
+        print("*" * 10, "\nStart server \nCompleted [OK]")
+
+    def startFactory(self):
+        """
+        Запуск процесса ожидания новых клиентов
+        :return:
+        """
+        print("\n\nStart listening for the clients...")
+
+    def buildProtocol(self, addr):
+        """
+        Инициализация нового клиента
+        :param addr:
+        :return:
+        """
+        return Client(self)
+
+    def notify_all_users(self, data: str):
+        """
+        Отправка сообщений всем текущим пользователям
+        :param data:
+        :return:
+        """
+        for user in self.clients:
+            user.transport.write(f"{data}\n".encode())
+
+
+if __name__ == '__main__':
+    reactor.listenTCP(7410, Chat())
+    reactor.run()
